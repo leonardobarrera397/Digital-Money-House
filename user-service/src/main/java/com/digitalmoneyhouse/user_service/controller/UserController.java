f3a9513f3e4a6aded250a37192e1a5cd7bc1fe30
Leonardo Barrera <barreraleonardo230.com>
2024-11-25 21:12:01 -0300

    Gateway configurations

--- user-service/src/main/java/com/digitalmoneyhouse/user_service/controller/UserController.java
+++ user-service/src/main/java/com/digitalmoneyhouse/user_service/controller/UserController.java
@@ -7,10 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -32,4 +29,28 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
+
+    @GetMapping("/prueba")
+    public ResponseEntity<UserResponseDTO> prueba(@RequestHeader("Origen") String origen) {
+        UserResponseDTO userResponseDTO = new UserResponseDTO();
+        userResponseDTO.setId(1L);
+        userResponseDTO.setDni("12345678");
+        userResponseDTO.setFirstName("instancia 2");
+        userResponseDTO.setLastName("Argento");
+        userResponseDTO.setEmail("pepe@email.com");
+        userResponseDTO.setPhone("123456789");
+        userResponseDTO.setCvu("0001234567890987654321");
+        userResponseDTO.setAlias("uno.dos.tres");
+
+        System.out.println("Enviado desde " + origen);
+
+        if (!origen.equals("gateway")){
+            return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(userResponseDTO);
+        }
+
+
+        return ResponseEntity.status(HttpStatus.OK).body(userResponseDTO);
+    }
+
+
 }
