6927a5729ca53f8c1fc7751d18a1662f1d6eee3a
Leonardo Barrera <barreraleonardo230.com>
2024-11-27 20:11:09 -0300

    Implementing Lombok in Gateway

--- gateway/src/main/java/com/digitalmoneyhouse/gateway/controller/GatewayController.java
+++ gateway/src/main/java/com/digitalmoneyhouse/gateway/controller/GatewayController.java
@@ -1,5 +1,6 @@
 package com.digitalmoneyhouse.gateway.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/gateway")
+@Slf4j
 public class GatewayController {
 
     @GetMapping("/admin")
